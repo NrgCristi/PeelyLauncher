70056f0c923dab0fc9e545769d0ae5bd080b8d4a
NrgCristi <97525096+NrgCristi@users.noreply.github.com>
2022-09-22 17:00:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,5 @@ os.system("pip install time")
 
 input('Please enter your authcode from https://rebrand.ly/authcode:')
 print("Launching Fortnite")
-os.startfile("com.epicgames.launcher://apps/fn%3A4fe75bbc5a674f4f9b356b5c90567da5%3AFortnite?action=launch&silent=true -AUTH_LOGIN=unused -AUTH_PASSWORD= -AUTH_TYPE=exchangecode -epicapp=Fortnite -epicenv=Prod -epicportal -epicuserid=")
-
-             
+args = ('-AUTH_LOGIN=unused, -AUTH_PASSWORD=exchange_code, -AUTH_TYPE=exchangecode, -epicapp=Fortnite, -epicenv-Prod, -EpicPortal')
+os.startfile('com.epicgames.launcher://apps/fn%3A4fe75bbc5a674f4f9b356b5c90567da5%3AFortnite?action=launch&silent=true + args')
