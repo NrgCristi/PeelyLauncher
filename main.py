5c40ca4d28c106c18f7762496db3e64de4eda8dd
NrgCristi <97525096+NrgCristi@users.noreply.github.com>
2022-09-22 13:16:18 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import time
+os.system("pip install os")
+os.system("pip install time")
+
+input('Please enter your authcode from https://rebrand.ly/authcode:')
+print("Launching Fortnite")
+os.startfile("com.epicgames.launcher://apps/fn%3A4fe75bbc5a674f4f9b356b5c90567da5%3AFortnite?action=launch&silent=true -AUTH_LOGIN=unused -AUTH_PASSWORD= -AUTH_TYPE=exchangecode -epicapp=Fortnite -epicenv=Prod -epicportal -epicuserid=")
+
+             
