0ce6b17a6ec663c4edc93064d373526ea1eacc7c
NrgCristi <97525096+NrgCristi@users.noreply.github.com>
2022-09-23 12:35:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,9 @@ import os
 import time
 os.system("pip install os")
 os.system("pip install time")
-
-input('Please enter your authcode from https://rebrand.ly/authcode:')
-print("Launching Fortnite")
-args = ('-AUTH_LOGIN=unused, -AUTH_PASSWORD=exchange_code, -AUTH_TYPE=exchangecode, -epicapp=Fortnite, -epicenv-Prod, -EpicPortal')
-os.startfile('com.epicgames.launcher://apps/fn%3A4fe75bbc5a674f4f9b356b5c90567da5%3AFortnite?action=launch&silent=true + args')
-else:
-    {
-    	
-    	if len(input) != 32:
-        	print("The auth code is incorrect and it must be 32 characters long, please get a new one from https://rebrand.ly/authcode")
-}
+code = ('Please enter your authorization code from rebrand.ly/authcode')
+print('Launching Fortnite')
+os.startfile('com.epicgames.launcher://apps/fn%3A4fe75bbc5a674f4f9b356b5c90567da5%3AFortnite?action=launch&silent=true -AUTH_LOGIN=unused -AUTH_PASSWORD={input} -AUTH_TYPE=input -epicapp=Fortnite -epicenv=Prod -EpicPortal')
+while False:
+            if len(code) != 32:
+                print("The Auth code you provided is not valid and it must be 32 characters long, please get a new one from rebrand.ly/authcode")
