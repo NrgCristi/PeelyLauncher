fc7d2402da88fce41ef2c9bec24f32ef2e08c9a4
NrgCristi <97525096+NrgCristi@users.noreply.github.com>
2022-09-23 15:03:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import os
 import time
 os.system("pip install os")
 os.system("pip install time")
-code = ('Please enter your authorization code from rebrand.ly/authcode')
-print('Launching Fortnite')
-os.startfile('com.epicgames.launcher://apps/fn%3A4fe75bbc5a674f4f9b356b5c90567da5%3AFortnite?action=launch&silent=true -AUTH_LOGIN=unused -AUTH_PASSWORD={input} -AUTH_TYPE=input -epicapp=Fortnite -epicenv=Prod -EpicPortal')
+user_code = input('Please Enter your auth code from rebrand.ly/authcode:')
 while False:
-            if len(code) != 32:
-                print("The Auth code you provided is not valid and it must be 32 characters long, please get a new one from rebrand.ly/authcode")
+    if len(user_code) != 32:
+        {
+            print('the auth code you provided is incorrect, invalid, expired, or not 32 characters long, please get a new one from rebrand.ly/authcode any try again')
+        }
+
+
+while True:
+    {
+        os.startfile('com.epicgames.launcher://apps/fn%3A4fe75bbc5a674f4f9b356b5c90567da5%3AFortnite?action=launch&silent=true -AUTH_LOGIN=unused -AUTH_PASSWORD={user_code} -AUTH_TYPE={user_code} -epicapp=Fortnite -epicenv=Prod -EpicPortal')
+    }
