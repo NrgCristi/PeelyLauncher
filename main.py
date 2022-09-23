a3732c2cf56396137c0c5000b23155dab931e7f9
NrgCristi <97525096+NrgCristi@users.noreply.github.com>
2022-09-23 10:02:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ input('Please enter your authcode from https://rebrand.ly/authcode:')
 print("Launching Fortnite")
 args = ('-AUTH_LOGIN=unused, -AUTH_PASSWORD=exchange_code, -AUTH_TYPE=exchangecode, -epicapp=Fortnite, -epicenv-Prod, -EpicPortal')
 os.startfile('com.epicgames.launcher://apps/fn%3A4fe75bbc5a674f4f9b356b5c90567da5%3AFortnite?action=launch&silent=true + args')
+else:
+    {
+    	
+    	if len(input) != 32:
+        	print('The auth code is incorrect and it must be 32 characters long, please get a new one from https://rebrand.ly/authcode')
+}
