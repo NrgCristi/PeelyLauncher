a8d3a5d70ee805853d3505bde23eb27d7d332cb5
NrgCristi <97525096+NrgCristi@users.noreply.github.com>
2022-09-29 07:57:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ if len(user_code) != 32:
 else:
     if len(user_code) == 32:
         print('Launching Fortnite')
-        os.startfile("com.epicgames.launcher://apps/fn%3A4fe75bbc5a674f4f9b356b5c90567da5%3AFortnite?action=launch&silent=true -AUTH_LOGIN=unused -AUTH_PASSWORD={exchange_code} -AUTH_TYPE=exchangecode -epicapp=Fortnite -epicenv=Prod -EpicPortal")
+        os.startfile("com.epicgames.launcher://apps/fn%3A4fe75bbc5a674f4f9b356b5c90567da5%3AFortnite?action=launch&silent=true -AUTH_LOGIN=unused -AUTH_PASSWORD={user_code} -AUTH_TYPE=exchangecode -epicapp=Fortnite -epicenv=Prod -EpicPortal")
         print('Closing In 1 Minute')
         time.sleep(60)
